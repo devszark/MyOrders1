6a40625cbdfb84d8f8c78b02f90a51ffe3b2b773
devszark <devszark@gmail.com>
2022-11-22 17:09:07 +0100

    Order.addItem now works with Streams

--- src/main/java/entity/Order.java
+++ src/main/java/entity/Order.java
@@ -25,24 +25,55 @@ public class Order {
         this.description = description;
     }
 
-    public void addItem(OrderItem orderItem){
-        for (OrderItem oI : items){
-            if (oI.getProduct().equals(orderItem.getProduct())){
-                oI.setQuantity(oI.getQuantity()+orderItem.getQuantity());
+    public void addItem(OrderItem newOrderItem){
+ /*       for (OrderItem oI : items){
+            if (oI.getProduct().equals(newOrderItem.getProduct())){
+                oI.setQuantity(oI.getQuantity()+newOrderItem.getQuantity());
                 return;
             }
         }
-        items.add(orderItem);
+        items.add(newOrderItem);*/
+
+        // Find existing OrderItem which holds the same product as in orderItem
+        OrderItem existingItem = items.stream()
+                .filter(item -> item.getProduct().equals(newOrderItem.getProduct()))
+                .findFirst()
+                .orElse(null);
+        if (existingItem!=null){
+            // if found, just add quantity
+            existingItem.setQuantity(existingItem.getQuantity()+ newOrderItem.getQuantity());
+        } else{
+            // if not found, just add another OrderItem
+            items.add(newOrderItem);
+        }
     }
 
-    public void addItem(Product newP, int quantity){
-        for (OrderItem oI : items){
+    /**
+     Adding a new product as a product item
+     If the product already exists on the item list - then add only quantity
+     */
+    public void addItem(Product newP, int quantity)
+     {
+        /*for (OrderItem oI : items){
             if (oI.getProduct().equals(newP)){
                 oI.setQuantity(oI.getQuantity()+quantity);
                 return;
             }
+          items.add(new OrderItem(newP,quantity));
+        }*/
+
+        // Find existing OrderItem which holds newP product already
+        OrderItem existingItem = items.stream()
+                .filter(item -> item.getProduct().equals(newP))
+                .findFirst()
+                .orElse(null);
+        if (existingItem!=null){
+            // if found, just add quantity
+            existingItem.setQuantity(existingItem.getQuantity()+quantity);
+        } else{
+            // if not found, just add another OrderItem
+            items.add(new OrderItem(newP,quantity));
         }
-        items.add(new OrderItem(newP,quantity));
     }
 
 
