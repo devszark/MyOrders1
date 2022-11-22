2bda37aed5114afd5a1f56c994b5b4ed3888fb8a
devszark <devszark@gmail.com>
2022-11-22 17:10:40 +0100

    Order.addItem simplified

--- src/main/java/entity/Order.java
+++ src/main/java/entity/Order.java
@@ -26,26 +26,7 @@ public class Order {
     }
 
     public void addItem(OrderItem newOrderItem){
- /*       for (OrderItem oI : items){
-            if (oI.getProduct().equals(newOrderItem.getProduct())){
-                oI.setQuantity(oI.getQuantity()+newOrderItem.getQuantity());
-                return;
-            }
-        }
-        items.add(newOrderItem);*/
-
-        // Find existing OrderItem which holds the same product as in orderItem
-        OrderItem existingItem = items.stream()
-                .filter(item -> item.getProduct().equals(newOrderItem.getProduct()))
-                .findFirst()
-                .orElse(null);
-        if (existingItem!=null){
-            // if found, just add quantity
-            existingItem.setQuantity(existingItem.getQuantity()+ newOrderItem.getQuantity());
-        } else{
-            // if not found, just add another OrderItem
-            items.add(newOrderItem);
-        }
+        addItem(newOrderItem.getProduct(), newOrderItem.getQuantity());
     }
 
     /**
